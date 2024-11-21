92a903709310f452f18e0301443337478dd3daa2
Mzwandile Dlomo <world.mjdlomo@gmail.com>
2024-11-21 10:31:03 +0200

    twillio working, waiting for varified acc

--- libs/sendSms.ts
+++ libs/sendSms.ts
@@ -1,13 +1,23 @@
 // Import the Twilio library using ES module syntax
 import twilio from 'twilio';
 
-const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+const sid = process.env.TWILIO_ACCOUNT_SID;
+
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+const phone_number = process.env.TWILIO_PHONE_NUMBER;
+
+const client = twilio(sid, authToken);  
+
+// const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 export async function sendSMS(phone: string, code: string) {
     try {
+      console.log(`Sending SMS to ${phone} with code ${code}`);
+
       const message = await client.messages.create({
         body: `Your verification code is: ${code}`,
-        from: process.env.TWILIO_PHONE_NUMBER,
+        from: phone_number,
         to: phone,
       });
       return `Message sent successfully! SID: ${message.sid}`;
